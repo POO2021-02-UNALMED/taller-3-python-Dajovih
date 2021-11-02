9d34429043e239145c43fe0174376113c2ef7ea3
Dajovih <80296654+Dajovih@users.noreply.github.com>
2021-11-02 17:11:45 -0500

    Creación de paquete

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class Marcas:
+    def __init__(self,nombre):
+        self._nombre=nombre
+    
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self,n_nombre):
+        self._nombre=n_nombre
--- televisores/tv.py
+++ televisores/tv.py
@@ -1,17 +1,3 @@
-from _typeshed import Self
-
-
-class Marcas:
-    def __init__(self,nombre):
-        self._nombre=nombre
-    
-    def getNombre(self):
-        return self._nombre
-    
-    def setNombre(self,n_nombre):
-        self._nombre=n_nombre
-
-
 class tv:
     _numTV=0
     def __init__(self,marca,estado):
@@ -91,39 +77,3 @@ class tv:
         if (self._estado==True):
             if self._volumen!=0:
                 self._volumen-=1
-    
-class Control:
-    def __init__(self):
-        self._tv=None
-    
-    def turnOn(self):
-        self._tv.turnOn()
-
-    def turnOff(self):
-        self._tv.turnOff()
-
-    def canalUp(self):
-        self._tv.canalUp()
-
-    def canalDown(self):
-        self._tv.canalDown()
-
-    def volumenUp(self):
-        self._tv.volumenUp()
-
-    def volumenDown(self):
-        self._tv.volumenDown()
-
-    def setCanal(self,n_canal):
-        self._tv.setCanal(n_canal)
-
-
-    def enlazar(self,tele):
-        self._tv=tele
-        self._tv.setControl(self)
-    
-    def getTv(self):
-        return self._tv
-
-    def setTv(self,n_tv):
-        self._tv=n_tv
